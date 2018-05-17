1e33794fe4fc1118b4aa2464f16e05495ca64b55
Zack YL Shih <zackyl.shih@moxa.com>
2018-05-17 09:54:13 -0700

    Modify for old gcc version 3.3.2

--- src/common.h
+++ src/common.h
@@ -23,6 +23,53 @@
 #ifndef _COMMON_H
 #define _COMMON_H
 
+#include <stdint.h>
+
+#ifdef __USE_BSD
+/* Conversion interfaces.  */
+# include <byteswap.h>
+
+# if __BYTE_ORDER == __LITTLE_ENDIAN
+#  define htobe16(x) __bswap_16 (x)
+#  define htole16(x) (x)
+#  define be16toh(x) __bswap_16 (x)
+#  define le16toh(x) (x)
+
+#  define htobe32(x) __bswap_32 (x)
+#  define htole32(x) (x)
+#  define be32toh(x) __bswap_32 (x)
+#  define le32toh(x) (x)
+
+#  define htobe64(x) __bswap_64 (x)
+#  define htole64(x) (x)
+#  define be64toh(x) __bswap_64 (x)
+#  define le64toh(x) (x)
+# else
+#  define htobe16(x) (x)
+#  define htole16(x) __bswap_16 (x)
+#  define be16toh(x) (x)
+#  define le16toh(x) __bswap_16 (x)
+
+#  define htobe32(x) (x)
+#  define htole32(x) __bswap_32 (x)
+#  define be32toh(x) (x)
+#  define le32toh(x) __bswap_32 (x)
+
+#  define htobe64(x) (x)
+#  define htole64(x) __bswap_64 (x)
+#  define be64toh(x) (x)
+#  define le64toh(x) __bswap_64 (x)
+# endif
+#endif
+
+extern int interactive;
+extern int write_immed;
+extern int atari_format;	/* Use Atari variation of MS-DOS FS format */
+
+/* program_name used for printing messages; no name will be printed when it is
+ * left as NULL */
+extern const char *program_name;
+
 void die(const char *msg, ...)
     __attribute((noreturn, format(printf, 1, 2)));
 
